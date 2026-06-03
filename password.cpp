3e35bed76bd07239506a73e130ad43bfb0799716
Bode <289588@burlcoschools.org>
2026-06-03 13:07:07 -0400

    Update password.cpp

--- password.cpp
+++ password.cpp
@@ -1,19 +1,46 @@
-// Example program
 #include <iostream>
 #include <string>
 using namespace std;
 
+// delcaring functions
+void intro(string& password); // intro function
+void checks(bool& uppercase, bool& lowercase, bool& number, string& password); // checking function
+void errors(string& password, bool& correctLength, bool& number, bool& uppercase, bool& lowercase, bool& validPass); // errors function
+void validation(string& password, string& password2, bool& validPass);
+
 int main()
 {
-    string password;
-    int i = 0;
-    bool number, uppercase, lowercase, validPass;
+    // declaring variables
+    string password, password2;
+    bool number, uppercase, lowercase;
+    bool validPass = 0;
     bool correctLength = 0;
-    cout << "-----------------------------------------------------\n";
+    
+    // do while loop
+    do {
+    intro(password);
+    
+    checks(uppercase, lowercase, number, password);
+    
+    errors(password, correctLength, number, uppercase, lowercase, validPass);
+    
+    } while (!validPass);
+    
+    validation(password, password2, validPass);
+    
+    };
+    
+// defining functions
+void intro(string& password) { // intro function
+        cout << "-----------------------------------------------------\n";
     cout << "Please enter a password. The password must contain:\n - at least 8 characters\n - at least one number\n - one uppercase letter\n - one lowercase letter.\n";
     cout << "-----------------------------------------------------\n";
     getline(cin, password);
-    
+    cout << "-----------------------------------------------------\n";
+}
+
+void checks(bool& uppercase, bool& lowercase, bool& number, string& password) { // checking function
+    int i = 0;
     do {
         if (isupper(password[i])) {
             uppercase = true;
@@ -25,11 +52,13 @@ int main()
         
         i++;
     } while (i < password.length());
-    
+}
+
+void errors(string& password, bool& correctLength, bool& number, bool& uppercase, bool& lowercase, bool& validPass) { // error function
     if (password.length() >= 8) {
         correctLength = true;
     };
-    if (uppercase == true && lowercase == true && number == true && correctLength == true) {
+    if (uppercase == true &&lowercase == true && number == true && correctLength == true) {
         validPass = true;
     };
     if (!uppercase) {
@@ -43,5 +72,17 @@ int main()
     }
     if (!correctLength) {
         cout << "You need 8 letters\n";
-    };
+    }; 
+}
+
+void validation(string& password, string& password2, bool& validPass) { // validation function
+    if (validPass) {
+        cout << "Your password is valid! Please verify it by typing it again: ";
+        getline(cin, password2);
+        } while (password2!= password) {
+            cout << "That's wrong, enter it again\n";
+            getline(cin, password2);
+        }
+        cout << "-----------------------------------------------------\n";
+        cout << "Password verified";
 }
